027de0c32a6312bef53470e214fafbbcd39d0ed2
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-06-15 16:59:15 +0530

    Book(getmapping) --> done

--- src/main/java/com/library/Book_Library/Book.java
+++ src/main/java/com/library/Book_Library/Book.java
@@ -1,11 +1,25 @@
 package com.library.Book_Library;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/book")
 public class Book {
+    Map<Integer, Book_Entry> Book_Entry_Map = new HashMap<>();
+
+    @GetMapping
+    public List<Book_Entry> bookEntry(){
+        return  new ArrayList<>(Book_Entry_Map.values());
+    }
+
+
 
 
 }
