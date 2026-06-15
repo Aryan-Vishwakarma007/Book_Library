1d771b79b19978752777a6b5b8fdedf8580179fa
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-06-15 16:04:23 +0530

    Health_Check --> done

--- src/main/java/com/library/Book_Library/Health_Check.java
+++ src/main/java/com/library/Book_Library/Health_Check.java
@@ -1,4 +1,12 @@
 package com.library.Book_Library;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Health_Check {
+    @GetMapping("/check")
+    public String check(){
+        return "Health --> OK";
+    }
 }
